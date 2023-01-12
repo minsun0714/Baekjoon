1a00e3390773bda9737d39c0906321288a4918ca
minsun0714 <woooooooow22@gmail.com>
2023-01-12 20:55:50 +0900

    ⚰️ 불필요한 파일

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-const input = require("fs").readFileSync("example.txt").toString().trim();
-const n = Number(input);
-let cards = [...Array(n)].map((_, i) => i + 1);
-let discard = [];
-while (cards.length > 1) {
-  discard.push(cards[0]);
-  cards.shift();
-  cards.push(cards[0]);
-  cards.shift();
-}
-const ans = [...discard, ...cards].join(" ");
-console.log(ans);
+const input = require("fs")
+  .readFileSync("example.txt")
+  .toString()
+  .trim()
+  .split("\n");
