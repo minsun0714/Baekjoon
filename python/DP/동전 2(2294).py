3678a92afc7bab434ccb707c3bb8d4da23ea8e6e
jasmine0714 <woooooooow22@gmail.com>
2023-10-15 20:26:48 +0900

    🔥 불필요한 set 제거

--- python/DP/동전 2(2294).py
+++ python/DP/동전 2(2294).py
@@ -4,7 +4,6 @@ s = open("input.txt", "rt")  # 주석 처리해야 하는 부분
 
 n, k = map(int, s.readline().split())
 coin_type = [int(s.readline()) for _ in range(n)]
-coin_type = list(set(coin_type))
 
 dp = [10001] * (k + 1)  # 동전을 가장 많이 사용하는 경우 1원으로 10000원을 만들 수 있으므로 10001로 초기화
 dp[0] = 0
