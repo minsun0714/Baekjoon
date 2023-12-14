f2e105eb0b9c1b15ba568ce18e067066ec296f8b
jasmine0714 <woooooooow22@gmail.com>
2023-12-14 20:58:35 +0900

    ⚡️ DFS와 BFS(1260) : 답 출력 방식 개선

--- python/탐색/DFS와 BFS(1260).py
+++ python/탐색/DFS와 BFS(1260).py
@@ -6,33 +6,30 @@ s = open("input.txt", "rt")  # 주석 처리해야 하는 부분
 n, m, v = map(int, s.readline().split())
 
 graph = [[] for _ in range(n + 1)]
-edges = [list(map(int, s.readline().split())) for _ in range(m)]
 
 for _ in range(m):
-    [a, b] = edges.pop(0)
-    graph[a].append(b)
-    graph[b].append(a)
+    [node_1, node_2] = list(map(int, s.readline().split()))
+    graph[node_1].append(node_2)
+    graph[node_2].append(node_1)
 
-for el in graph:
-    el.sort()
+for node in graph:
+    node.sort()
 
 
 def dfs():
     stack = [v]
-    visited = [0] * (n + 1)
-    dfs_answer = ""
+    visited = [False] * (n + 1)
+    answer = []
 
     while stack:
-        current_node = stack.pop()
-        if (visited[current_node]):
-            continue
-        dfs_answer += str(current_node) + " "
-        visited[current_node] = 1
-        for i in graph[current_node][::-1]:
-            if visited[i] == 0:
+        cur_node = stack.pop()
+        if not visited[cur_node]:
+            visited[cur_node] = True
+            answer.append(cur_node)
+            for i in graph[cur_node][::-1]:
                 stack.append(i)
 
-    print(dfs_answer)
+    print(*answer)
 
 
 dfs()
@@ -40,20 +37,18 @@ dfs()
 
 def bfs():
     q = deque([v])
-    visited = [0] * (n + 1)
-    bfs_answer = ""
+    visited = [False] * (n + 1)
+    answer = []
 
     while q:
-        current_node = q.popleft()
-        if (visited[current_node]):
-            continue
-        bfs_answer += str(current_node) + " "
-        visited[current_node] = 1
-        for i in graph[current_node]:
-            if visited[i] == 0:
+        cur_node = q.popleft()
+        if not visited[cur_node]:
+            visited[cur_node] = True
+            answer.append(cur_node)
+            for i in graph[cur_node]:
                 q.append(i)
 
-    print(bfs_answer)
+    print(*answer)
 
 
 bfs()
