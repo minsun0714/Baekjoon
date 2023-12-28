7f8349eb3a0fb2cf33657cb274cb3588896e5e78
jasmine0714 <woooooooow22@gmail.com>
2023-12-28 21:05:45 +0900

    ✅ 계단 오르기(2579) : 복습

--- python/DP/계단 오르기(2579).py
+++ python/DP/계단 오르기(2579).py
@@ -2,41 +2,21 @@ from sys import stdin as s
 
 s = open("input.txt", "rt")  # 주석 처리해야 하는 부분
 
-n = int(s.readline())
+n = int(s.readline().strip())
+stairs = [int(s.readline().strip()) for _ in range(n)]
 
-stairs = [0] * 302
+if n < 3:
+    print(sum(stairs))
+    exit(0)
 
-for i in range(1, n + 1):
-    stairs[i] = int(s.readline())
+dp = [0] * n
+dp[0] = stairs[0]
 
-# if (n < 3):
-#     print(sum(stairs))
-#     exit(0)
+for i in range(1, n):
+    option1 = dp[i-3] + stairs[i-1]
+    option2 = dp[i-2]
+    # 마지막 계단을 반드시 밟아야 하기 때문에 현재 계단을 밟지 않는 optiond은 고려하지 않는다.
 
-dp = [0] * 302
-dp[1] = stairs[1]
-dp[2] = stairs[1] + stairs[2]
-
-for i in range(3, n + 1):
-    # 2가지 option이 있다.
-    # # 이전 계단을 밟고 이번 계단을 밟는 것.
-    # option1 = dp[i-3] + max(stairs[i-2], stairs[i-1]) + stairs[i]
-    # # 이번 계단을 포기하고 다음 계단을 밟는 것.
-    # option2 = dp[i-3] + stairs[i-2] + stairs[i - 1] + stairs[i + 1]
-    # # 이전 계단을 포기하고 이번 계단을 밟는 것.
-    # if (option1 > option2):
-    #     # 이번 계단을 밟는다. (직전 2개 중 하나를 버린다.)
-    #     dp[i] = option1
-    # else:
-    #     # 이번 계단을 밟지 않는다.
-    #     dp[i] = dp[i-1]
-
-    # 2가지 option이 있다.
-    # 전제: i번째 계단을 밟는다.
-    # i-1 번째 계단 포기
-    option1 = dp[i-2]
-    # i-2 번째 계단 포기
-    option2 = dp[i-3] + stairs[i-1]
     dp[i] = max(option1, option2) + stairs[i]
 
-print(dp[n])
+print(dp[n-1])
