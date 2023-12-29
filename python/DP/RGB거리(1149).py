a74ad955376ac23d64e25f578dd9da51e0981255
jasmine0714 <woooooooow22@gmail.com>
2023-12-29 22:06:19 +0900

    ✅ RGB거리(1149) 복습

--- python/DP/RGB거리(1149).py
+++ python/DP/RGB거리(1149).py
@@ -3,23 +3,11 @@ from sys import stdin as s
 s = open("input.txt", "rt")  # 주석 처리해야 하는 부분
 
 n = int(s.readline().strip())
-
-cost = []
-
-for i in range(n):
-    cost.append(list(map(int, s.readline().strip().split())))
+houses = [list(map(int, s.readline().split())) for _ in range(n)]
 
 for i in range(1, n):
-    for j in range(3):
-        min_left, min_right = 1e9, 1e9
-        left = cost[i - 1][: j]
-        right = cost[i - 1][j + 1: n]
+    houses[i][0] += min(houses[i-1][1], houses[i-1][2])
+    houses[i][1] += min(houses[i-1][0], houses[i-1][2])
+    houses[i][2] += min(houses[i-1][0], houses[i-1][1])
 
-        if left:
-            min_left = min(left)
-        if right:
-            min_right = min(right)
-
-        cost[i][j] += min(min_left, min_right)
-
-print(min(cost[n - 1]))
+print(min(houses[n-1]))
