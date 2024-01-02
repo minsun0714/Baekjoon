a2b9b28f23951872c9d532063d7a5fafbed9ad93
jasmine0714 <woooooooow22@gmail.com>
2024-01-02 21:56:18 +0900

    ✅ 바이러스(2606): 복습

--- python/탐색/바이러스(2606).py
+++ python/탐색/바이러스(2606).py
@@ -3,31 +3,30 @@ from collections import deque
 
 s = open("input.txt", "rt")  # 주석 처리해야 하는 부분
 
-n = int(s.readline())
-m = int(s.readline())
+n = int(s.readline().strip())
+
+l = int(s.readline().strip())
 
 graph = [[] for _ in range(n + 1)]
+
+for i in range(l):
+    [x, y] = list(map(int, s.readline().split()))
+    graph[x].append(y)
+    graph[y].append(x)
+
+q = deque([1])
 visited = [False] * (n + 1)
-
-nodes = [[key, value] for key, value in [
-    list(map(int, s.readline().split())) for _ in range(m)]]
-
-for node in nodes:
-    graph[node[0]].append(node[1])
-    graph[node[1]].append(node[0])
-
 visited[1] = True
-queue = deque([1])
 
-while queue:
-    cur = queue.popleft()
-    for node in graph[cur]:
-        if not visited[node]:
-            visited[node] = True
-            queue.append(node)
+count = 0
 
-true_count = 0
-for i in range(2, n + 1):
-    if visited[i]:
-        true_count += 1
-print(true_count)
+while q:
+    cur = q.popleft()
+
+    for i in graph[cur]:
+        if not visited[i]:
+            visited[i] = True
+            q.append(i)
+            count += 1
+
+print(count)
